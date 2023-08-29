beaf4436789c6a268c26e31bdd1e01ccef921cb1
huda.uul <syamsul.huda@weup.jp>
2023-08-29 22:25:31 +0700

    day 21: snake part 2

---
+++ 21-snake-2/main.py
@@ -0,0 +1,59 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.title("My Snake Game")
+screen.bgcolor("black")
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+# important to trigger the listen() method here
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+# move the snake
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.2)
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        # create snake food 
+        food.refresh()
+        snake.extend()
+        # create a scoreboard
+        scoreboard.increase_score()
+
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
